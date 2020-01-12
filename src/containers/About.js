6d8c92a33a0f1c756f7bf0ef37fb700d527b5e9a
yulineko <yulineko@gmail.com>
2020-01-12 17:35:50 -0500

    contact form

--- src/containers/About.js
+++ src/containers/About.js
@@ -1,6 +1,13 @@
 import React from 'react'
 
 class About extends React.Component{
+    state = {
+        name: null,
+        email: null,
+        subject: null,
+        message: null
+    }
+    
     render(){
         return(
             <div>
@@ -8,7 +15,11 @@ class About extends React.Component{
                 Contact: 
                 <div className = "contact">
                     <form>
-                        
+                        <input type = "text" placeholder = "name" name = "name" value = {this.state.name} onChange /><br/>
+                        <input type = "text" placeholder = "email" name = "email" value = {this.state.email} onChange /><br/>
+                        <input type = "text" placeholder = "subject" name = "subject" value = {this.state.subject} onChange /><br/>
+                        <textarea type = "text" placeholder = "message" name = "message" value = {this.state.message} onChange /><br/>
+                        <input type="submit"/>
                     </form>
                 </div>
             </div>
