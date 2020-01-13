3ceb3ce1a085367a662bc1b322f19ae561ec702a
yulineko <yulineko@gmail.com>
2020-01-12 20:10:19 -0500

    date + time

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -3,36 +3,45 @@ import React from 'react'
 class Home extends React.Component{
     state = {
         time: "00:00:00",
+        date: null
     }
 
     getTime = () => {
         const addZero = n => n < 10 ? "0" +  n : n
+        let d = new Date()
 
-        setInterval(() => {
-            let date = new Date()
-            let hour = addZero(date.getHours())
-            let min = addZero(date.getMinutes())
-            let sec = addZero(date.getSeconds())
-            let time = `${hour}:${min}:${sec}`
+        let day = d.getDate()
+        let month = d.getMonth()
+        let year = d.getFullYear()
+        
+        let hour = addZero(d.getHours())
+        let min = addZero(d.getMinutes())
+        let sec = addZero(d.getSeconds())
 
-            this.setState({
-                time: time
-            })
-        }, 1000)
+        let time = `${hour}:${min}:${sec}`
+        let date = `${day} ${month} ${year}`
+
+
+        this.setState({
+            time: time, 
+            date: date
+        })
     }
 
     componentDidMount(){
-        this.loadInterval = setInterval(
-            this.getTime, 1000
-        )
+        this.getTime()
+        this.interval = setInterval(this.getTime, 1000);
     }
 
     render(){
-        // console.log("TIME:", this.state)
+        // console.log("TIME:", this.state.time)
+        // console.log("DATE:", this.state.date)
 
         return(
             <div>
                 <div className={this.state.time === "00:00:00" ? "time blink" : "time"}> 
+                    {this.state.date}
+                    <br/>
                     {this.state.time}
                 </div>
             </div>
