b50d8cfff3d9fb92118f2206523786b662649c03
yulineko <yulineko@gmail.com>
2020-01-13 14:58:43 -0500

    positions of planets

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,13 +1,23 @@
 import React from 'react'
+import SolarSystem from 'solaris-model'
 
 class Home extends React.Component{
     state = {
         time: "00:00:00",
-        date: null
+        date: null,
+    }
+
+    componentDidMount(){
+        this.getTime()
+        this.interval = setInterval(this.getTime, 1000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval);
     }
 
     getTime = () => {
-        const addZero = n => n < 10 ? "0" +  n : n
+        const addZero = (n) => n < 10 ? "0" +  n : n
         let d = new Date()
 
         let day = d.getDate()
@@ -55,6 +65,8 @@ class Home extends React.Component{
             case 12:
                 month = "December"
                 break
+            default:
+                break
         }
 
         let time = `${hour}:${min}:${sec}`
@@ -62,18 +74,58 @@ class Home extends React.Component{
 
         this.setState({
             time: time, 
-            date: date
+            date: date,
         })
     }
 
-    componentDidMount(){
-        this.getTime()
-        this.interval = setInterval(this.getTime, 1000);
+    getSolarSystem = () => {
+        let system = new SolarSystem()
+        
+        let sun = system.bodies.sun
+        let mercury = system.bodies.mercury
+        let venus = system.bodies.venus
+        let earth = system.bodies.earth
+        let mars = system.bodies.mars 
+        let jupiter = system.bodies.jupiter 
+        let saturn = system.bodies.saturn 
+        let uranus = system.bodies.uranus 
+        let neptune = system.bodies.neptune 
+        let pluto = system.bodies.pluto
+
+        let sunPosition = sun.position
+        let mercuryPosition = mercury.position
+        let venusPosition = venus.position
+        let earthPosition = earth.position
+        let marsPosition = mars.position
+        let jupiterPosition = jupiter.position 
+        let saturnPosition = saturn.position
+        let uranusPosition = uranus.position
+        let neptunePosition = neptune.position
+        let plutoPosition = pluto.position
+
+        // this.setState({
+            // sun: sunPosition,
+            // mercury: mercuryPosition,
+            // venus: venusPosition,
+            // earth: earthPosition,
+            // mars: marsPosition,
+            // jupiter: jupiterPosition,
+            // saturn: saturnPosition,
+            // uranus: uranusPosition,
+            // neptune: neptunePosition,
+            // pluto: plutoPosition
+        // })
+
+        return earthPosition
     }
+
+
 
     render(){
         // console.log("TIME:", this.state.time)
         // console.log("DATE:", this.state.date)
+        // console.log("TIME:", this.state)
+        // console.log("EARTH POSITION: ", this.getSolarSystem())
 
         return(
             <div>
@@ -81,6 +133,8 @@ class Home extends React.Component{
                     {this.state.date}
                     <br/>
                     {this.state.time}
+                    <br/>
+                    {this.getSolarSystem()}
                 </div>
             </div>
         )
