c479ba33dd90bb9bf1f68edbf8368af7fd430ee4
yulineko <yulineko@gmail.com>
2020-01-12 19:48:47 -0500

    clock

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,10 +1,40 @@
 import React from 'react'
 
 class Home extends React.Component{
+    state = {
+        time: "00:00:00",
+    }
+
+    getTime = () => {
+        const addZero = n => n < 10 ? "0" +  n : n
+
+        setInterval(() => {
+            let date = new Date()
+            let hour = addZero(date.getHours())
+            let min = addZero(date.getMinutes())
+            let sec = addZero(date.getSeconds())
+            let time = `${hour}:${min}:${sec}`
+
+            this.setState({
+                time: time
+            })
+        }, 1000)
+    }
+
+    componentDidMount(){
+        this.loadInterval = setInterval(
+            this.getTime, 1000
+        )
+    }
+
     render(){
+        // console.log("TIME:", this.state)
+
         return(
             <div>
-                clock
+                <div className={this.state.time === "00:00:00" ? "time blink" : "time"}> 
+                    {this.state.time}
+                </div>
             </div>
         )
     }
