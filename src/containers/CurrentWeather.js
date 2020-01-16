beb454b8b923eb8ce64446f44bf7631cc0c9e41c
yulineko <yulineko@gmail.com>
2020-01-15 23:29:37 -0500

    working on gettin geolocation

--- src/containers/CurrentWeather.js
+++ src/containers/CurrentWeather.js
@@ -1,10 +1,26 @@
 import React from 'react'
 
+const DS_KEY = process.env.REACT_APP_DS_KEY
+
 class CurrentWeather extends React.Component{
     state = {
-        temperature: "0"
+        loc: null,
+        currTemp: null,
     }
-    rener(){
+
+
+    getGeolocation = () => {
+        navigator.geolocation.getCurrentPosition((location) => {
+            console.log(location.coords.latitude);
+            console.log(location.coords.longitude);
+            console.log(location.coords.accuracy);
+        });
+    }
+
+
+    render(){
+        console.log(this.getGeolocation())
+        
         return(
             <div>
 
