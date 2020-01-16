661fa080caae5910e2c063f186e5a077c6cfd7ee
yulineko <yulineko@gmail.com>
2020-01-15 21:48:08 -0500

    cuurent weather widget setup

---
+++ src/containers/CurrentWeather.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class CurrentWeather extends React.Component{
+    rener(){
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default CurrentWeather
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Clock from './Clock'
 import Sol from './Sol'
+import CurrentWeather from './CurrentWeather'
 
 class Home extends React.Component{
     render(){
         return(
             <div>
                 <Clock />
+                <CurrentWeather />
                 <Sol />
             </div>
         )
